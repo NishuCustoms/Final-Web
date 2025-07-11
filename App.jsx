9b94addbc248049a33a4e9ff16e3772fc06f4d00
NishuCustoms <nishukustoms@gmail.com>
2025-07-11 17:24:35 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 text-2xl">
+      Nishu Customs is live!
+    </div>
+  );
+}
+
+export default App;
